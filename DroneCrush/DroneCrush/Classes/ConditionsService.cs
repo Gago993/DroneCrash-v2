071f7e7ad32d7d5964f2ec9de996c044000e45c2
Dejan Bogatinovski <bogatinovski.dejan@gmail.com>
2016-04-23 17:43:40 +0200

    Calculate positive flight conditions probability

---
+++ DroneCrush/DroneCrush/Classes/ConditionsService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DroneCrush.Classes
+{
+    public class ConditionsService
+    {
+        public List<string> GetCloudyConditions()
+        {
+            return new List<string>() { "cloudy", "mostly cloudy (night)", "partly cloudy", "mostly cloudy (day)", "partly cloudy (night)", "partly cloudy (day)" };
+        }
+
+        public List<string> GetExtremeConditions()
+        {
+            return new List<string>() { "tornado", "tropical storm", "hurricane", "severe thunderstorms", "thunderstorms" };
+        }
+
+        public List<string> GetFoggyConditions()
+        {
+            return new List<string>() { "foggy", "haze", "smoky", "blustery", "dust", "windy", "cold" };
+        }
+
+        public List<string> GetSnowConditions()
+        {
+            return new List<string>() { "mixed snow and sleet", "snow showers", "freezing drizzle", "freezing rain", "heavy snow", "scattered snow showers", "snow flurries", "light snow showers", "blowing snow", "snow", "hail", "sleet" };
+        }
+
+        public List<string> GetClearConditions()
+        {
+            return new List<string>() { "clear (night)", "sunny", "not available", "fair (night)", "fair (day)", "hot" };
+        }
+
+        public List<string> GetRainConditions()
+        {
+            return new List<string>() { "mixed rain and snow", "isolated thundershowers", "mixed rain and sleet", "thundershowers", "drizzle", "showers", "mixed rain and hail", "isolated thunderstorms", "scattered thunderstorms", "scattered showers" };
+        }
+    }
+}
--- DroneCrush/DroneCrush/Controllers/WebApi/GetStatusInfoController.cs
+++ DroneCrush/DroneCrush/Controllers/WebApi/GetStatusInfoController.cs
@@ -1,4 +1,5 @@
-﻿using DroneCrush.DataContext;
+﻿using DroneCrush.Classes;
+using DroneCrush.DataContext;
 using DroneCrush.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -78,9 +79,71 @@ namespace DroneCrush.Controllers.WebApi
             return Ok(model);
         }
 
+        // humidity (0 - 100) %
+        // windSpeed mph
+        // visibility  miles
+        // numberOfDrones drones
+        [Route("api/GetStatusInfo/Probability")]
+        public double GetProbability(double humidity = 10, double windSpeed = 50, double visibility = 1, int numberOfDrones = 40, string condition = "Sunny")
+        {
+            ConditionsService conditionsService = new ConditionsService();
+            double result = 0;
 
+            // Humidity
+            if (humidity >= 0 && humidity <= 20 || humidity > 80 && humidity <= 100)
+                result += 0.1 * 80;
+            else if (humidity > 20 && humidity < 40 || humidity >= 60 && humidity <= 80)
+                result += 0.1 * 90;
+            else result += 0.1 * 100;
 
+            // Wind speed
+            if (windSpeed == 0)
+                result += 0.3 * 100;
+            else if (windSpeed <= 5)
+                result += 0.3 * 85;
+            else if (windSpeed <= 10)
+                result += 0.3 * 60;
+            else if (windSpeed <= 25)
+                result += 0.3 * 20;
+            else if (windSpeed <= 40)
+                result += 0.3 * 5;
+            else result += 0;
 
+            // Visibility
+            if (visibility <= 1)
+                result += 0.2 * 5;
+            else if (visibility <= 5)
+                result += 0.2 * 60;
+            else if (visibility <= 12)
+                result += 0.2 * 90;
+            else result += 0.2 * 100;
 
+            // Number of Drones
+            if (numberOfDrones == 0)
+                result += 0.2 * 100;
+            else if (numberOfDrones < 3)
+                result += 0.2 * 70;
+            else if (numberOfDrones < 8)
+                result += 0.2 * 40;
+            else if (numberOfDrones < 15)
+                result += 0.2 * 10;
+            else result += 0;
+
+            // Weather conditions
+            if (conditionsService.GetCloudyConditions().Contains(condition))
+                result += 0.2 * 80;
+            else if (conditionsService.GetExtremeConditions().Contains(condition))
+                result += 0.2 * 0;
+            else if (conditionsService.GetFoggyConditions().Contains(condition))
+                result += 0.2 * 10;
+            else if (conditionsService.GetSnowConditions().Contains(condition))
+                result += 0.2 * 40;
+            else if (conditionsService.GetRainConditions().Contains(condition))
+                result += 0.2 * 30;
+            else if (conditionsService.GetClearConditions().Contains(condition))
+                result += 0.2 * 100;
+
+            return result;
+        }
     }
 }
